51d06e676817e98915ecb896d6e952e81d79dfca
Fernando Muñoz <fernando1986caballeria@gmail.com>
2025-04-21 15:55:22 -0500

    modificacion script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,9 +2,14 @@
 fetch("pages/navbar.html")
     .then(res => res.text())
     .then(data => {
-        document.getElementById("navbar").innerHTML = data;
+        document.getElementById("navbar-fija").innerHTML = data;
 });
 
+fetch("pages/footer.html")
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById("footer").innerHTML = data;
+});
 
 document.getElementById("boton-form").addEventListener('click', function () {
     document.getElementById("respuesta").textContent = "Gracias por contactarnos, pronto nos comunicaremos contigo."
